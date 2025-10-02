92342d0e24a08db57af0a9b4f508f154b5721c52
AbuZafor99 <zaforsadiq987@gmail.com>
2025-10-02 14:04:02 +0600

    commit with company pricing pages

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 import 'package:karlfive/core/init/app_initializer.dart';
 import 'package:karlfive/core/theme/app_theme.dart';
-import 'package:karlfive/features/user_pricing/presentation/screens/plan_pricing_screen.dart';
 import 'features/auth/presentation/screens/splash_screen.dart';
 import 'package:karlfive/core/common/constants/stripe_key.dart';
+
+import 'features/company_pricing/presentation/screens/plan_pricing_screen.dart';
 
 void main() async {
   await AppInitializer.initializeApp();
