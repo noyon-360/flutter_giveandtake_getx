5aed2025a1bc2710312c38129f98d8c9915f0cbf
Iftikhar Shams Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2025-10-07 14:45:30 +0600

    Update otp_request_model_register.dart

--- lib/features/auth/data/models/otp_request_model_register.dart
+++ lib/features/auth/data/models/otp_request_model_register.dart
@@ -2,15 +2,9 @@ class OtpRequestModelRegister {
   final String email;
   final String otp;
 
-  OtpRequestModelRegister({
-    required this.email,
-    required this.otp,
-  });
+  OtpRequestModelRegister({required this.email, required this.otp});
 
   Map<String, dynamic> toJson() {
-    return {
-      'email': email,
-      'otp': otp,
-    };
+    return {'email': email, 'otp': otp};
   }
 }
