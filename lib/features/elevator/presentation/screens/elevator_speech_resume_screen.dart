13b3f963cc2cca7ff4548b8df41aa4a29c55a0e9
Iftikhar Shams Niloy <81116328+Iftikhar-Shams-Niloy@users.noreply.github.com>
2025-10-09 13:07:39 +0600

    Add AboutWidget and SkillsWidget; refactor ElevatorSpeechResumeScreen layout

--- lib/features/elevator/presentation/screens/elevator_speech_resume_screen.dart
+++ lib/features/elevator/presentation/screens/elevator_speech_resume_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:karlfive/features/elevator/presentation/widgets/about_widget.dart';
 import 'package:karlfive/features/elevator/presentation/widgets/contact_card.dart';
+import 'package:karlfive/features/elevator/presentation/widgets/skill_widget.dart';
 
 import '../../../../core/theme/app_colors.dart';
 import '../widgets/title_with_edit_logo.dart';
@@ -13,78 +15,98 @@ class ElevatorSpeechResumeScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text("Elevator Speech Resume")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Column(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 30),
-              TitleWithEditLogo(
-                title: "Elevator Speech Resume",
-                onPress: () {
-                  //* <--- Handle edit action --->
-                },
-              ),
-              SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 72.0),
-                child: Text(
-                  "Get a quick glimps of my work and creative process through this video portfolio",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
-                ),
-              ),
-              SizedBox(height: 20),
-              Center(
-                child: SizedBox(
-                  width: screenWidth - (16 * 2),
-                  height: 105,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Container(
-                      color: Colors.black,
-                      child: Stack(
-                        fit: StackFit.expand,
-                        children: [
-                          Center(
-                            child: Icon(
-                              Icons.play_circle_fill,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
+              Column(
+                children: [
+                  SizedBox(height: 30),
+                  TitleWithEditLogo(
+                    title: "Elevator Speech Resume",
+                    onPress: () {
+                      //* <--- Handle edit action --->
+                    },
+                  ),
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 72.0),
+                    child: Text(
+                      "Get a quick glimps of my work and creative process through this video portfolio",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
                   ),
-                ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: SizedBox(
+                      width: screenWidth - (16 * 2),
+                      height: 105,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Container(
+                          color: Colors.black,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Center(
+                                child: Icon(
+                                  Icons.play_circle_fill,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  TitleWithEditLogo(
+                    title: "My Resume",
+                    onPress: () {
+                      //* <--- Handle edit action --->
+                    },
+                  ),
+                  SizedBox(height: 16),
+
+                  ContactCard(),
+
+                  SizedBox(height: 16),
+
+                  Divider(
+                    color: AppColors.textGrey,
+                    thickness: 1,
+                    indent: 16,
+                    endIndent: 16,
+                  ),
+
+                  SizedBox(height: 16),
+
+                  AboutWidget(),
+
+                  SizedBox(height: 16),
+
+                  Divider(
+                    color: AppColors.textGrey,
+                    thickness: 1,
+                    indent: 16,
+                    endIndent: 16,
+                  ),
+
+                  SizedBox(height: 16),
+
+                  SkillsWidget(),
+                ],
               ),
-              SizedBox(height: 16),
-              TitleWithEditLogo(
-                title: "My Resume",
-                onPress: () {
-                  //* <--- Handle edit action --->
-                },
-              ),
-              SizedBox(height: 16),
-
-              ContactCard(),
-
-              SizedBox(height: 24),
-
-              Divider(
-                color: AppColors.textGrey,
-                thickness: 1,
-                indent: 16,
-                endIndent: 16,
-              ),
-
-              SizedBox(height: 16),
-
-              // AboutCard(),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
