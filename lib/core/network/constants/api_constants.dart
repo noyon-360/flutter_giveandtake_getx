d59e59fc219a9cc25167d85fa87215c3eecbf251
Soykot Rahman Rodro <soykotrahman1212@gmail.com>
2025-10-28 12:53:20 +0600

    run locally

--- lib/core/network/constants/api_constants.dart
+++ lib/core/network/constants/api_constants.dart
@@ -1,9 +1,10 @@
 class ApiConstants {
   /// [Base Configuration]
-  static const String baseDomain = 'https://api.evpitch.com';
+     static const String baseDomain = 'http://10.10.5.91:5001';  /// soykot ip
+  // static const String baseDomain = 'https://api.evpitch.com';
   static const String baseUrl = '$baseDomain/api/v1';
 
-  // static const String soyDomain = 'http://10.10.5.91:5000';  /// soykot ip
+
 
   // static const String baseDomain = 'https://api.evpitch.com';
   // static const String baseDomain = 'http://10.10.5.3:5000'; // iftikhar
