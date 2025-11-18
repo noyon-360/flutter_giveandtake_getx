135d9efc4f738106e0097ba18a345f1799ae8f43
Md Abu Zafor <138440583+AbuZafor99@users.noreply.github.com>
2025-11-18 15:07:19 +0600

    commit after merge with eshita

--- lib/core/network/constants/api_constants.dart
+++ lib/core/network/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   /// [Base Configuration]
-  //    static const String baseDomain = 'http://10.10.5.91:5001';  /// soykot ip
-  static const String baseDomain = 'https://api.evpitch.com';
+     static const String baseDomain = 'http://10.10.5.88:5001';  /// soykot ip
+  // static const String baseDomain = 'https://api.evpitch.com';
   static const String baseUrl = '$baseDomain/api/v1';
 
   // add by zafor
