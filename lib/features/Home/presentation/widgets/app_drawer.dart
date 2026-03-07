c2af861c95ddd5aa69362849ffd14289334c96b2
Sristy72 <eshitamondol72@gmail.com>
2026-03-07 12:02:40 +0600

    public view done

--- lib/features/Home/presentation/widgets/app_drawer.dart
+++ lib/features/Home/presentation/widgets/app_drawer.dart
@@ -9,6 +9,7 @@ import 'package:giveandtake/features/elevator/presentation/controller/resume_che
 import 'package:giveandtake/features/home_static_screens/data/models/contactus_model.dart';
 import 'package:giveandtake/features/home_static_screens/presentation/screen/contact_us_screen.dart';
 import 'package:giveandtake/features/job_listing/presentation/screens/bookmark_jobs_screen.dart';
+import 'package:giveandtake/features/recruiter_account/presentation/screens/recruiter_public_view.dart';
 
 import '../../../company/data/model/seach_all_user_response_model.dart';
 import '../../../company/presentation/controller/company_details_controller.dart';
@@ -206,7 +207,7 @@ class _AppDrawerState extends State<AppDrawer> {
                                     children: [
                                       Flexible(
                                         child: Text(
-                                          user.name ?? 'Unknown',
+                                          user.name,
                                           maxLines: 1,
                                           overflow: TextOverflow.ellipsis,
                                           style: const TextStyle(
@@ -278,7 +279,7 @@ class _AppDrawerState extends State<AppDrawer> {
                                       ],
                                     ],
                                   ),
-                                  subtitle: Text(user.address ?? ''),
+                                  subtitle: Text(user.address),
                                   // trailing: const Icon(Icons.person_outline),
                                   onTap: () {
                                     final slug = user.slug;
@@ -298,7 +299,16 @@ class _AppDrawerState extends State<AppDrawer> {
                                           slug: slug,
                                         ),
                                       );
-                                    } else {
+                                    }
+                                    else if(user.role.toLowerCase() ==
+                                        'recruiter'){
+                                      Get.to(
+                                            () => RecruiterPublicViewScreen(
+                                          slug: slug,
+                                        ),
+                                      );
+                                    }
+                                    else {
                                       Get.to(
                                         () => PublicViewSeachScreen(slug: slug),
                                       );
